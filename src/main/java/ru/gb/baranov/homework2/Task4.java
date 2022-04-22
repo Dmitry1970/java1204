9bd59487ddae955b91bf5d85218f957e0d1f25d3
Dmitry <badl1970@mail.ru>
2022-04-22 19:37:04 +0300

    Вынес в отдельный метод. Можно ли сделать через метод while, как я указал в закомментированных строках

--- src/main/java/ru/gb/baranov/homework2/Task4.java
+++ src/main/java/ru/gb/baranov/homework2/Task4.java
@@ -2,11 +2,18 @@ package ru.gb.baranov.homework2;
 
 public class Task4 {
     public static void main(String[] args) {
-        String a = "Строка";
-        int value = 5;
-        while (value > 0) {
-            System.out.println(a);
-            value--;
+        printTextNTimes("строка", 5);
+    }
+
+    public static void printTextNTimes(String text, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(text);
         }
     }
 }
+//        while (n > 0) {
+//            System.out.println(text);
+//            n--;
+//        }
+
+
