76cacc602db1c01cca0cef8fe2fc705c9e1bc1c5
Dmitry <badl1970@mail.ru>
2022-04-22 19:33:33 +0300

    Изменил название метода

--- src/main/java/ru/gb/baranov/homework2/Task2.java
+++ src/main/java/ru/gb/baranov/homework2/Task2.java
@@ -3,11 +3,10 @@ package ru.gb.baranov.homework2;
 public class Task2 {
 
     public static void main(String[] args) {
-        int a = -5;
-        PositiveOrNegative(a);
+        printExpression(8);
     }
 
-    public static void PositiveOrNegative(int value) {
+    public static void printExpression(int value) {
         if (value >= 0) {
             System.out.println("Число " + value + " положительное");
         } else {
